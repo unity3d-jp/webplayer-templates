3dbd74f8aa5cef5ba8660e6fba6295a02e62a3b9
Keigo Ando <anchan828@gmail.com>
2014-06-22 13:14:27 +0900

    supported image, title , description

---
+++ Assets/TemplatesEditorScript/Editor/WebPlayerTemplatesPostProcessBuild.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEditor.Callbacks;
+
+public class WebPlayerTemplatesPostProcessBuild
+{
+	[PostProcessBuild]
+	public static void OnPostprocessBuild (BuildTarget target, string pathToBuiltProject)
+	{
+		if (target == BuildTarget.WebPlayer) {
+			Application.CaptureScreenshot (pathToBuiltProject + "/thumbnail.png");
+		}
+	}
+}
